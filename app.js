ea0d1b4cdeaa0e45e26f54e4f3f9779378948507
emfrab <56005158+emfrab@users.noreply.github.com>
2021-06-17 10:26:08 -0300

    Test Axios - FormData (#4)
    * Creado servidor, axios en test aparte
    
    * Archivo se sube, sigue tirando error
    
    * Upload terminado
    
    * Comentarios eliminados

--- app.js
+++ app.js
@@ -4,31 +4,44 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const app = express();
 
 
 const uploader = async (req, res) => {
     try {
         await upload(req, res)
 
-        if (req.fil == undefined) {
+        if (req.file == undefined) {
             return res.status(400).send({message: 'Por favor elija un archivo'})
         }
 
         res.status(200).send({message: `Archivo subido: ${req.file.originalname}`})
+        console.log('Archivo subido')
     }catch(err) {
         console.log(err)
     }
-}; 
+};
 
-app.listen(3000, () => {
-    console.log("Running");
-});
+function build({port}) {
+    console.log('Creating server')
+    const app = express();
+    
+    app.post('/upload', uploader)
+    app.get("/download", (req, res) => {
+        res.sendFile(__dirname + "/files/cat.gif")
+    })
 
-app.get("/", (req, res) => {
-    res.json("Running");
-});
+    return new Promise((resolve, reject) => {
+        const server = app
+        .listen(port)
+        .once('error', () => {
+            reject(new Error('Error al conectarse'))
+        })
+        .once('listening', () => {
+            console.log('Running...')
+            server.port = server.address().port
+            resolve(server)
+        });
+    });
+};
 
-app.get("/download", (req, res) => {
-    res.sendFile(__dirname + "/files/cat.gif")
-})
+export {build}
